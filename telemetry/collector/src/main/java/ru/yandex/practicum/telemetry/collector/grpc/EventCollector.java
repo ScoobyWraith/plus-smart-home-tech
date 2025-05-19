e5677028bc4fedf7878ef1ef214d19954810804a
m.kiselev <sanwiraight@gmail.com>
2025-05-19 21:03:11 +0300

    [Sprint 20] Add hub handler

--- telemetry/collector/src/main/java/ru/yandex/practicum/telemetry/collector/grpc/EventCollector.java
+++ telemetry/collector/src/main/java/ru/yandex/practicum/telemetry/collector/grpc/EventCollector.java
@@ -22,7 +22,6 @@ public class EventCollector extends CollectorControllerImplBase {
     private final Map<SensorEventProto.PayloadCase, GRPCSensorEventHandler> sensorEventHandlers;
     private final Map<HubEventProto.PayloadCase, GRPCHubEventHandler> hubEventHandlers;
 
-
     @Autowired
     public EventCollector(Set<GRPCSensorEventHandler> sensorEventHandlers, Set<GRPCHubEventHandler> hubEventHandlers) {
         this.sensorEventHandlers = sensorEventHandlers.stream()
@@ -42,6 +41,22 @@ public class EventCollector extends CollectorControllerImplBase {
         try {
             if (sensorEventHandlers.containsKey(request.getPayloadCase())) {
                 sensorEventHandlers.get(request.getPayloadCase()).handle(request);
+            } else {
+                throw new IllegalArgumentException("Не могу найти обработчик для события " + request.getPayloadCase());
+            }
+
+            responseObserver.onNext(Empty.getDefaultInstance());
+            responseObserver.onCompleted();
+        } catch (Exception e) {
+            responseObserver.onError(new StatusRuntimeException(Status.fromThrowable(e)));
+        }
+    }
+
+    @Override
+    public void collectHubEvent(HubEventProto request, StreamObserver<Empty> responseObserver) {
+        try {
+            if (hubEventHandlers.containsKey(request.getPayloadCase())) {
+                hubEventHandlers.get(request.getPayloadCase()).handle(request);
             } else {
                 throw new IllegalArgumentException("Не могу найти обработчик для события " + request.getPayloadCase());
             }
