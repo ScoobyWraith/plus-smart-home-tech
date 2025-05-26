a279c73f7f4024d0fd6e5098b7a2944960c6f21c
m.kiselev <sanwiraight@gmail.com>
2025-05-26 19:44:48 +0300

    [Sprint 20] Init analyzer

---
+++ telemetry/analyzer/src/main/java/ru/yandex/practicum/telemetry/analyzer/service/HubEventProcessor.java
@@ -0,0 +1,11 @@
+package ru.yandex.practicum.telemetry.analyzer.service;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class HubEventProcessor implements Runnable {
+    @Override
+    public void run() {
+
+    }
+}
---
+++ telemetry/analyzer/src/main/java/ru/yandex/practicum/telemetry/analyzer/service/SnapshotProcessor.java
@@ -0,0 +1,10 @@
+package ru.yandex.practicum.telemetry.analyzer.service;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class SnapshotProcessor {
+    public void start() {
+
+    }
+}
