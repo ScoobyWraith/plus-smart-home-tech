b0a953097ef1d7a13a8c3fa9fe803ef6ec1dd8b7
Max <sanwiraight@gmail.com>
2025-06-13 20:50:12 +0300

    [Sprint 21] Fix annotations

--- commerce/shopping-store/src/main/java/ru/yandex/practicum/shopping_store/ShoppingStore.java
+++ commerce/shopping-store/src/main/java/ru/yandex/practicum/shopping_store/ShoppingStore.java
@@ -4,7 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 
-@EnableFeignClients
+@EnableFeignClients(basePackages = {"ru.yandex.practicum.interaction_api.interaction"})
 @SpringBootApplication
 public class ShoppingStore {
     public static void main(String[] args) {
